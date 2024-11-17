1de4a11d1ad29be2d64e94cdd3dd147f7ad74951
Ted Senft <code@robotgryphon.com>
2024-11-17 18:40:52 -0500

    Add build scripts and versioning

--- spatial/build.gradle.kts
+++ spatial/build.gradle.kts
@@ -1,6 +1,11 @@
+var envVersion: String = System.getenv("VERSION") ?: "0.0.1"
+if (envVersion.startsWith("v"))
+    envVersion = envVersion.trimStart('v')
+
 plugins {
     id("idea")
     id("java")
+    id("maven-publish")
 
     alias(neoforged.plugins.moddev)
 }
@@ -17,4 +22,23 @@ java {
 
 neoForge {
     neoFormVersion = neoforged.versions.neoform
+}
+
+
+val PACKAGES_URL = System.getenv("GH_PKG_URL") ?: "https://maven.pkg.github.com/compactmods/spatial"
+publishing {
+    publications.register<MavenPublication>("spatial") {
+        from(components.getByName("java"))
+    }
+
+    repositories {
+        // GitHub Packages
+        maven(PACKAGES_URL) {
+            name = "GitHubPackages"
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
